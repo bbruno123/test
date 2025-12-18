dfd30841ff08cc350af4a774e584bf2697421928
bbruno123 <bruno60835396@aluno.es.senac.br>
2025-12-18 19:58:28 +0000

    update

--- testes/5/game_main.js
+++ testes/5/game_main.js
@@ -7,58 +7,190 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+let cubeRotation = true;
+let cubeColorRGB = true;
+
+//Loop principal//
+function animate() {
+    requestAnimationFrame(animate);
+
+    updateDebugUI();
+    debugMenuStatus();
+
+    // bulletSpeed(0.02);
+    
+    
+    if (cubeRotation === true){
+        rotationCube();
+    }else{
+        //Não chama;
+    }
+    
+    renderer.render(scene, camera);
+}
+
+scene.fog = new THREE.Fog(0xffffff, 0.15, 50);
+
+//Menu de debug//
 const coordsElement = document.querySelector("#coords");
 const debug_ui = document.querySelector("#debug_ui");
 
-document.addEventListener("keydown", (event)=>{
-    if (event.key === "F3" && !event.repeat){
+window.addEventListener("keydown", (event) => {
+    if (event.repeat) return;
+
+    //Mostra o menu de debug ao apertar F3//
+    if (event.key === "F3" && event.shiftKey) {
         event.preventDefault();
         debug_ui.classList.toggle("hidden");
     }
 
+    //Toggle a rotação do cubo//
+    if (event.key === "F4" && debugAtivo === true){
+        cubeRotation = !cubeRotation;
+    }
+
+    //Toggle a cor do cubo//
+    if (event.key === "4" && debugAtivo === true){
+        cubeColorRGB = !cubeColorRGB;
+    }
+
 });
 
+const geometryBullet = new THREE.BoxGeometry(0.3, 0.3, 0.3);
+const materialBullet = new THREE.MeshBasicMaterial({color: 0xfcf803});
+const bullet = new THREE.Mesh(geometryBullet, materialBullet);
+
+const cameraPosition = camera.position;
+console.log(cameraPosition);
+
+function bulletSpeed(bullet_speed){
+    // bullet.position.z -= bullet_speed;
+}
+
+document.addEventListener("mousedown", (event) => {
+    //Tiro//
+    if (event.button === 0) {
+        // scene.add(bullet);
+    }
+});
+
+document.addEventListener("keyup", (event) => {
+
+});
+
+//Funciona só se o menu de dubug estiver ativo//
+let debugAtivo = false;
+
+function debugMenuStatus(){
+    if (!debug_ui.classList.contains("hidden")){
+        debugAtivo = true;
+
+    }else{
+        debugAtivo = false;
+    }
+}
+//Pega as coordenadas//
 function updateDebugUI(){
-    coordsElement.innerText = `x: ${camera.position.x.toFixed(2)}` + " | " + `y: ${camera.position.y.toFixed(2)}` + " | " + `z: ${camera.position.z.toFixed(2)}`;
+    coordsElement.innerText = `x: ${player.position.x.toFixed(2)}` + " | " + `y: ${player.position.y.toFixed(2)}` + " | " + `z: ${player.position.z.toFixed(2)}`;
 }
+//Menu de debug//
 
-function animate() {
-    requestAnimationFrame(animate);
-    updateDebugUI();
-    
-    renderer.render(scene, camera);
-}
-animate();
-
+//Cubo doido//
 const geometryCube = new THREE.BoxGeometry(1, 1, 1);
-const materialCube = new THREE.MeshBasicMaterial({color: 0x00ff00});
+const materialCube = new THREE.MeshBasicMaterial({color: 0xfcf803});
 const cube = new THREE.Mesh(geometryCube, materialCube);
 
-cube.position.set(0, 0, 0);
+cube.position.set(0, 0, -4);
 scene.add(cube);
 
-camera.position.z = 5;
+//Rotação do cubo//
+let i = 0.005;
 
+function rotationCube(){
+    cube.rotation.y += i;
+    cube.rotation.x += i;
+    return;
+}
+//Rotação do cubo//
+
+//Toggle cubo rgb//
+let r = 0;
+let g = 0;
+let b = 0;
+
+let rgb = "";
+
+const interval = setInterval(() => {
+
+    if (cubeColorRGB === true){
+        r++;
+        // g++;
+        // b++;
+
+    }else{
+        //Não chama;
+    }
+
+    if (r > 255) r = 0;
+    if (g > 255) g = 0;
+    if (b > 255) b = 0;
+
+    rgb = `rgb(${r}, ${g}, ${b})`;
+
+    cube.material.color.set(rgb);
+
+
+}, 30); // 1000 ms entre cada passo
+
+const player = new THREE.Object3D();
+scene.add(player);
+
+player.add(camera);
+camera.position.set(0, 0, 0);
+
+const q = new THREE.Quaternion();
 
 //movimentação da câmera
-document.addEventListener("keypress", (event) => {
+window.addEventListener("keydown", (event) => {
+
+    const moveSpeed = 0.3;
+    const rotateSpeed = 0.06;
+    
+    if (event.key === "ArrowRight"){
+        q.setFromAxisAngle(new THREE.Vector3(0, 1, 0), -rotateSpeed);
+        player.quaternion.multiply(q);
+    }
+
+    if (event.key === "ArrowLeft"){
+        q.setFromAxisAngle(new THREE.Vector3(0, 1, 0), rotateSpeed);
+        player.quaternion.multiply(q);
+    }
+
+    const forward = new THREE.Vector3(0, 0, -1);
+    forward.applyQuaternion(player.quaternion);
+
+    const right = new THREE.Vector3(1, 0, 0);
+    right.applyQuaternion(player.quaternion);
 
     if (event.key.toLocaleLowerCase() === "w"){
-        camera.position.z -= 0.3;
+        player.position.x += forward.x * moveSpeed;
+        player.position.z += forward.z * moveSpeed;
     }
 
     if (event.key.toLocaleLowerCase() === "s"){
-        camera.position.z += 0.3;
+        player.position.x -= forward.x * moveSpeed;
+        player.position.z -= forward.z * moveSpeed;
     }
-
-    if (event.key.toLocaleLowerCase() === "a"){
-        camera.position.x -= 0.3;
-    }
-
+    
     if (event.key.toLocaleLowerCase() === "d"){
-        camera.position.x += 0.3;
+        player.position.x += right.x * moveSpeed;
+        player.position.z += right.z * moveSpeed;
+    }
+    
+    if (event.key.toLocaleLowerCase() === "a"){
+        player.position.x -= right.x * moveSpeed;
+        player.position.z -= right.z * moveSpeed;
     }
 
 });
-
-
+animate();
