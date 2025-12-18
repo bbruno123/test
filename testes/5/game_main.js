c1a5f8a804e36b6efc193c68f7c62896b5b77dbb
bbruno123 <bruno60835396@aluno.es.senac.br>
2025-12-18 20:30:49 +0000

    update

--- testes/5/game_main.js
+++ testes/5/game_main.js
@@ -3,8 +3,10 @@ import * as THREE from 'https://unpkg.com/three@0.168.0/build/three.module.js';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+ let divideResolution = 1;
+
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(window.innerWidth/divideResolution, window.innerHeight/divideResolution, false);
 document.body.appendChild(renderer.domElement);
 
 let cubeRotation = true;
